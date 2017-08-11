17df9ad41a49e78c9495ea39155de3f78e6fa57a
Juan Pablo Pelaez <onepelaez.sar@gmail.com>
2017-08-11 10:33:45 -0500

    Routing and Response actions

---
+++ app.js
@@ -0,0 +1,34 @@
+var url = require('url');
+var fs = require('fs');
+
+function renderHMTL(path, response) {
+    fs.readFile(path, null, function(error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File not found');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+}
+
+module.exports = {
+    handleRequest: function(request, response) {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+
+        var path = url.parse(request.url).pathname;
+        switch (path) {
+            case '/':
+                renderHMTL('./index.html', response);
+                break;
+            case '/login':
+                renderHMTL('./login.html', response);
+                break;
+            default:
+                response.writeHead(404);
+                response.write('Route not defined');
+                response.end();
+        }
+    }
+};
